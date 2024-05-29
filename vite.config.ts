9b3ee04b776e827b99d4c8d101aed373e9455b10
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-05-30 00:10:58 +0200

    Explain vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// This enables absolute imports like `import { useOidc } from "oidc";`
+// instead of `import { useOidc } from "../../oidc";`
+// You also need to add "compilerOptions": { "baseUrl": "src" } in your tsconfig.json for it to work
 import tsconfigPaths from "vite-tsconfig-paths";
 import { viteEnvs } from "vite-envs";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
