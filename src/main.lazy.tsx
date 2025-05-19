00a8a2afcb52f6fccb2f1cbd904be7573d0d646a
garronej <joseph.garrone@protonmail.com>
2025-05-19 16:41:00 +0200

    checkpoint

--- src/main.lazy.tsx
+++ src/main.lazy.tsx
@@ -9,7 +9,12 @@ import { createDsfrCustomBrandingProvider } from "@codegouvfr/react-dsfr/mui";
 import { createTheme } from "@mui/material/styles";
 import { useLang } from "i18n";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { GlobalStyles } from "tss";
+import { fr } from "@codegouvfr/react-dsfr";
 import logoInseePngUrl from "assets/logo-insee.png";
+import { useTheme } from "@mui/material/styles";
+import { darken } from "@mui/material/styles";
+
 import "assets/geist/main.css";
 
 const queryClient = new QueryClient();
@@ -80,10 +85,36 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
             <DsfrCustomBrandingProvider>
                 <OidcProvider>
                     <I18nFetchingSuspense>
-                        <RouterProvider router={router} />
+                        <>
+                            <Global />
+                            <RouterProvider router={router} />
+                        </>
                     </I18nFetchingSuspense>
                 </OidcProvider>
             </DsfrCustomBrandingProvider>
         </QueryClientProvider>
     </React.StrictMode>
 );
+
+function Global() {
+    const theme = useTheme();
+
+    return (
+        <GlobalStyles
+            styles={{
+                [`.${fr.cx("fr-btn")}`]: {
+                    "--hover-tint": theme.palette.primary.dark,
+                    "--active-tint": darken(theme.palette.primary.main, 0.24),
+                    "&:hover, &:active": {
+                        color: theme.palette.primary.contrastText
+                    }
+                },
+                [`.${fr.cx("fr-input")}`]: {
+                    "&&&": {
+                        borderTopLeftRadius: `0px`
+                    }
+                }
+            }}
+        />
+    );
+}
